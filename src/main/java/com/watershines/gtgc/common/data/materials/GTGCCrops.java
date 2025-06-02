3d9a49bdd06292704c20a0bd6bcaea9403c2411b
WaterShines123 <ojmorriss@gmail.com>
2025-06-02 19:44:02 +0100

    More boring crop stuff

--- src/main/java/com/watershines/gtgc/common/data/materials/GTGCCrops.java
+++ src/main/java/com/watershines/gtgc/common/data/materials/GTGCCrops.java
@@ -21,13 +21,40 @@ public class GTGCCrops {
 
     static {
         RUBBER.setEnabled(false);
+        BRONZE.setEnabled(false);
+        BRASS.setEnabled(false);
+        GRAPHITE.setEnabled(false);
+        REDSTONE_ALLOY.setEnabled(false);
+        CONDUCTIVE_ALLOY.setEnabled(false);
+        CONSTANTAN.setEnabled(false);
+        ELECTRUM.setEnabled(false);
+        INVAR.setEnabled(false);
+        ROSE_GOLD.setEnabled(false);
 
         SALTPETER.setTier(CropTier.ONE);
 
         GLOWSTONE.setTier(CropTier.TWO);
+        PRISMARINE.setTier(CropTier.TWO);
+        RABBIT.setTier(CropTier.TWO);
 
         ALUMINUM.setTier(CropTier.THREE);
         SILICON.setTier(CropTier.THREE);
+        LAPIS_LAZULI.setTier(CropTier.THREE);
+        END.setTier(CropTier.THREE);
+        BLAZE.setTier(CropTier.THREE);
+        GHAST.setTier(CropTier.THREE);
+        ENDERMAN.setTier(CropTier.THREE);
+        NICKEL.setTier(CropTier.THREE);
+        SAPPHIRE.setTier(CropTier.THREE);
+        PERIDOT.setTier(CropTier.THREE);
+
+        SILVER.setTier(CropTier.FOUR);
+        DIAMOND.setTier(CropTier.FOUR);
+        EMERALD.setTier(CropTier.FOUR);
+        WITHER_SKELETON.setTier(CropTier.FOUR);
+
+        TUNGSTEN.setTier(CropTier.FIVE);
+        OSMIUM.setTier(CropTier.FIVE);
 
         MOLYBDENUM = new Crop(new ResourceLocation("gtgc", "molybdenum"),
                 CropTier.THREE, CropType.RESOURCE,
